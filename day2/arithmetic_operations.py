fa559ad90b9cc4c385a54b4d0b2fa6b96112fdfb
YoungJin Kim <you359@sehan.ac.kr>
2025-02-25 21:28:36 +0900

    update python file

--- day2/arithmetic_operations.py
+++ day2/arithmetic_operations.py
@@ -17,6 +17,14 @@ def divide(a, b):
     return a / b
 
 
+def power(a, b):
+    return a ** b
+
+
+def mod(a, b):
+    return a % b
+
+
 # 함수 테스트 코드
 if __name__ == "__main__":
     # 테스트 값
@@ -28,6 +36,8 @@ if __name__ == "__main__":
     print(f"{x} - {y} = {subtract(x, y)}")
     print(f"{x} * {y} = {multiply(x, y)}")
     print(f"{x} / {y} = {divide(x, y)}")
+    print(f"{x} ** {y} = {power(x, y)}")
+    print(f"{x} % {y} = {mod(x, y)}")
 
     # 나누기 0 테스트
     print(f"{x} / 0 = {divide(x, 0)}")
